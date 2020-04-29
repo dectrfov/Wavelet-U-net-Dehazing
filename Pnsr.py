3ed314ebe7c69bedac7a8613224da106346a6e02
Danny_Yang <r05921014@ntu.edu.tw>
2020-04-29 23:37:20 +0800

    Update Pnsr.py

--- Pnsr.py
+++ Pnsr.py
@@ -9,17 +9,6 @@ def pnsr(im1,im2):
     n=im1.shape[1]
     mse = (im1-im2)**2
     mse=np.mean(mse)
-    '''    
-    if len(im1.shape)==2:
-        mse = (im1-im2)**2
-        mse=mse.sum()/m/n
-    if len(im1.shape)==3:
-        #print('fuck')
-        mse = (im1-im2)**2
-        mse=np.mean(mse)
-        #print(mse)
-    #print(rmse)
-    '''
     psnr = 10*np.log10(1/mse)
     return psnr
 
