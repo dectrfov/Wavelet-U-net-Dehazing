0272269f86d4b8e25c3b790e37fca95397925ecf
Danny_Yang <r05921014@ntu.edu.tw>
2020-04-29 23:36:12 +0800

    Update wavelet.py

--- wavelet.py
+++ wavelet.py
@@ -2,7 +2,6 @@
 import pywt
 import torch
 from torch.autograd import Variable
-from torchsummary import summary
 
 w=pywt.Wavelet('db1')
 
@@ -22,23 +21,20 @@ inv_filters = torch.stack([rec_lo.unsqueeze(0)*rec_lo.unsqueeze(1)*2.0,
                            rec_hi.unsqueeze(0)*rec_hi.unsqueeze(1)], dim=0)
 
 def wt(vimg):
-    #padded = torch.nn.functional.pad(vimg,(0,0,0,0)) 
-    padded=vimg
-    res=torch.zeros(vimg.shape[0],4*vimg.shape[1],int(vimg.shape[2]/2),int(vimg.shape[3]/2))
-    res=res.cuda()
+    padded = vimg
+    res = torch.zeros(vimg.shape[0],4*vimg.shape[1],int(vimg.shape[2]/2),int(vimg.shape[3]/2))
+    res = res.cuda()
     for i in range(padded.shape[1]):
         res[:,4*i:4*i+4] = torch.nn.functional.conv2d(padded[:,i:i+1], Variable(filters[:,None].cuda(),requires_grad=True),stride=2)
-        res[:,4*i+1:4*i+4]=(res[:,4*i+1:4*i+4]+1)/2.0
+        res[:,4*i+1:4*i+4] = (res[:,4*i+1:4*i+4]+1)/2.0
 
     return res
 
 def iwt(vres):
-    #vres=2*vres-1
-    res=torch.zeros(vres.shape[0],int(vres.shape[1]/4),int(vres.shape[2]*2),int(vres.shape[3]*2))
-    res=res.cuda()
+    res = torch.zeros(vres.shape[0],int(vres.shape[1]/4),int(vres.shape[2]*2),int(vres.shape[3]*2))
+    res = res.cuda()
     for i in range(res.shape[1]):
         vres[:,4*i+1:4*i+4]=2*vres[:,4*i+1:4*i+4]-1
         temp = torch.nn.functional.conv_transpose2d(vres[:,4*i:4*i+4], Variable(inv_filters[:,None].cuda(),requires_grad=True),stride=2)
-        #temp[]
-        res[:,i:i+1,:,:]=temp
+        res[:,i:i+1,:,:] = temp
     return res
